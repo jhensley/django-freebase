b692fe2c33827f9c1de1aa2449be22577730236b
Jonathan Hensley <jhensley@gannett.com>
2013-03-01 21:51:39 -0500

    downgraded version to 0.1dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='django-freebase',
-    version='0.1.0',
+    version='0.1dev',
     author='Jonathan Hensley',
     author_email='jhensley2@gmail.com',
     packages=['freebase'],
