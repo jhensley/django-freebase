5fc467c47d922bb20f8660fd2008cb3a6a00bd89
Jonathan Hensley <jhensley@gannett.com>
2013-03-01 21:38:39 -0500

    setting up the basics for packaging

---
+++ README.txt
@@ -0,0 +1,11 @@
+===============
+Django Freebase
+===============
+
+Django Freebase provides insanity at the moment... we'll see if it does anything more::
+
+
+Thanks to...
+------------
+
+Eric Moritz - Who hasn't helped yet, but is sure to be roped in soon
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='django-freebase',
+    version='0.1.0',
+    author='Jonathan Hensley',
+    author_email='jhensley2@gmail.com',
+    packages=['freebase'],
+    url='http://pypi.python.org/pypi/django-freebase/',
+    license='LICENSE.txt',
+    description='Freebasing in Django.',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "Django >= 1.5",
+    ],
+)
